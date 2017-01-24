6d3c83451816cbc44590f3781554e4b115a39cc5
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2017-01-24 21:39:53 -0200

    Inserção dos exemplos de códigos

--- arquivos/upload.php
+++ arquivos/upload.php
@@ -1,5 +1,4 @@
 <?php
-
 function validaArquivo($arquivo)
 {
     $extensao = explode(".", $arquivo);
@@ -14,14 +13,19 @@ function criarPasta($pasta)
     }
 }
 
+function moveArquivo($arquivo, $nomeDoArquivo)
+{
+    if(!file_exists($nomeDoArquivo)) {
+        return move_uploaded_file($arquivo, "files/{$nomeDoArquivo}.jpg");
+    }
+}
+
 if($_FILES) {
     if(validaArquivo($_FILES['arquivo']['name']))
     {
-        //Criar uma função para o upload
-        //Essa função deve validar se já existe o arquivo com o nome passado
-        //Se tudo estiver OK exibe em tela Arquivo enviado com sucesso!
         criarPasta('files');
-        $file = move_uploaded_file($_FILES['arquivo']['tmp_name'], 'files/teste.jpg');
-        var_dump($file);
+        if(moveArquivo($_FILES['arquivo']['tmp_name'], 'teste')) {
+            echo 'Arquivo criado com sucesso!';
+        }
     }
 }
